591d6aa51c9ca264408195abc7fa459cf9b137c4
Joris Osterhaus <joris@hardcode.nl>
2017-06-23 00:31:50 +0200

    added data handlers

--- read.py
+++ read.py
@@ -2,16 +2,20 @@
 
 import requests
 import sys
+import data_handlers
 
 def main():
-  if len(sys.argv) < 2:
-    print "usage: read.py <url>"
+  if len(sys.argv) < 3:
+    print "usage: read.py <url> <handler>"
   else:
     print str(sys.argv[1])
     resp = requests.get(sys.argv[1],verify=False)
-
     resp_json = resp.json()
-    print resp_json.dumps()
+    handler = getattr(data_handlers,sys.argv[2])
+    handler(resp_json)
+
+
+
 main()
 
 
