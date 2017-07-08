183e565b5cb70f506bc421c54f16faf9f3eac2b8
Joris Osterhaus <joris@hardcode.nl>
2017-07-09 00:16:54 +0200

    database

--- data_handlers.py
+++ data_handlers.py
@@ -1,10 +1,19 @@
-import sqlite3
 
-def tentoonstellingen(resp):
+def tentoonstellingen(resp,db_file_path):
 
   for t in resp:
     print t['trcid']
-    print t['title']
-    print t['location']['name']
-    print t['location']
+    title = t['title']
+    location = ", ".join([t['location']['name'], t['location']['adress'], t['location']['city']])
     print t['urls']
+    desc = " || ".join([t['details']["en"]["calendarsummary"],
+                        t['details']["en"]["shortdescription"],
+                        t['details']["nl"]["shortdescription"]] )
+    for u in t['urls']:
+      desc = desc + ' || ' + u
+
+    link = 'https://www.vvv.nl/nl/activiteitenkaart/detail/'+t['trcid']
+
+    
+    print [title,location,desc,link]
+
--- read.py
+++ read.py
@@ -3,17 +3,34 @@
 import requests
 import sys
 import data_handlers
+import sqlite3
 
 def main():
-  if len(sys.argv) < 3:
-    print "usage: read.py <url> <handler>"
+  prepare_tables(sys.argv[3])  
+  if len(sys.argv) < 4:
+    print "usage: read.py <url> <handler> <db_file_path>"
   else:
     print str(sys.argv[1])
     resp = requests.get(sys.argv[1],verify=False)
     resp_json = resp.json()
     handler = getattr(data_handlers,sys.argv[2])
-    handler(resp_json)
+    handler(resp_json,sys.argv[3])
+
+
+def prepare_tables(database_file_path):
+  db = sqlite3.connect(database_file_path)
+  db.execute('CREATE TABLE IF NOT EXISTS EventDates (Id TEXT PRIMARY KEY,Date TEXT)')
+  db.execute ("CREATE TABLE IF NOT EXISTS Event (Id TEXT PRIMARY KEY,Date TEXT,Time TEXT,"
+              "Img TEXT, Link TEXT,Title TEXT,Desc TEXT,Venue TEXT, Type TEXT default 'crawl')")
+  try:
+    db.execute ("ALTER TABLE Event ADD COLUMN IF NOT EXISTS VenuePlaintext TEXT");
+  except:
+    print "table alter skipped"
+  
+
+  db.execute ("CREATE INDEX IF NOT EXISTS idx1 ON Event(Venue)")
+  db.execute ("CREATE INDEX IF NOT EXISTS idx2 ON Event(Link)")
+  db.execute ("CREATE INDEX IF NOT EXISTS idx_date ON Event(Date)")
+
 
 main()
-
-
