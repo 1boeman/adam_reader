344bbf517ba17ea8f7948dfc881afd722e0a596c
1boeman <joriso@hardcode.nl>
2017-11-27 16:15:01 +0100

    completed tentoonstellingen handler

--- data_handlers.py
+++ data_handlers.py
@@ -1,31 +1,65 @@
+from datetime import date, timedelta 
+import hashlib
 
-def tentoonstellingen(resp,db_file_path):
-
+def tentoonstellingen(resp,db_file_path,venue_map):
+  events = []
   for t in resp:
-    print t['trcid']
-    title = t['title']
+   # print t['trcid']
+    event = {   
+      'title': t['title'],
+    }
+    
     location = ", ".join([t['location']['name'], t['location']['adress'], t['location']['city']])
-    print t['urls']
-    desc = " || ".join([t['details']["en"]["calendarsummary"],
+    
+    event['desc'] =  " || ".join( [location,t['details']["en"]["calendarsummary"],
                         t['details']["en"]["shortdescription"],
                         t['details']["nl"]["shortdescription"]] )
-    for u in t['urls']:
-      desc = desc + ' || ' + u
+   
+    event['location_tmp_id'] = "_".join([t['location']['name'].replace(" ","_"),t['location']['city'].replace(" ","_")])
+    event['genre'] = 1 
+
     if len(t['urls']):
       link = t['urls'][0]
+      for ur in t['urls']:
+        event['desc'] = event['desc'] + ' || ' + ur
+
     else:
       link = 'https://www.vvv.nl/nl/activiteitenkaart/detail/'+t['trcid']
-    print t['dates']
+
+    event['link'] = link
+     
+    if 'startdate' in t['dates']:
+      event['startdate'] = t['dates']['startdate']
+      event['id'] = get_id(t['title'],link,event['startdate'])
+      if 'enddate' in t['dates']:
+        event['enddate'] = t['dates']['enddate']
+    else: 
+      if 'singles' in t['dates']:
+        event['dates'] = t['dates']['singles']
+        event['id'] = get_id(t['title'],link,event['dates'][0])
+      else: 
+        print 'event has no dates:' + event['title'].encode('utf-8')  
+        
+    if 'media' in t:
+      event['img'] = t['media'][0]['url']
     
-    dates = get_dates_between()
-    print [title,location,desc,link]
+    if 'id' in event:
+      events.append(event)
+
+  save_events_to_db(events,db_file_path,venue_map)
+
+# generic function for saving parsed events to database
+def save_events_to_db(events,db_file_path, venue_map):
+  print db_file_path
+  print venue_map
+  #db = sqlite3.connect(database_file_path) 
+   
+ 
 
 
-def get_dates_between(start_date,end_date):
-    d1 = date(2008, 8, 15)  # start date
-  d2 = date(2008, 9, 15)  # end date
+def get_id(title,link,startdate):
+  pre_id = title+link+startdate
+  identifier = hashlib.sha224(pre_id.encode('utf-8')).hexdigest()
+  return identifier
 
-  delta = d2 - d1         # timedelta
 
-  for i in range(delta.days + 1):
-    print(d1 + timedelta(days=i))
--- read.py
+++ read.py
@@ -4,34 +4,48 @@ import requests
 import sys
 import data_handlers
 import sqlite3
+import os.path
 from datetime import date, timedelta
+import json
 
 def main():
-  prepare_tables(sys.argv[3])  
-  if len(sys.argv) < 4:
-    print "usage: read.py <url> <handler> <db_file_path>"
+  if len(sys.argv) < 5:
+    print "usage: read.py <url> <handler> <db_file_path> <path_to_venue_mappings.xml>"
   else:
+    prepare_tables(sys.argv[3])  
     print str(sys.argv[1])
     resp = requests.get(sys.argv[1],verify=False)
-    resp_json = resp.json()
+    #   print resp
+    #  resp_json = resp.json()
+    resp_json = json.loads(resp.content)
     handler = getattr(data_handlers,sys.argv[2])
-    handler(resp_json,sys.argv[3])
+    handler(resp_json,sys.argv[3],sys.argv[4])
 
 
 def prepare_tables(database_file_path):
-  db = sqlite3.connect(database_file_path)
-  db.execute('CREATE TABLE IF NOT EXISTS EventDates (Id TEXT PRIMARY KEY,Date TEXT)')
-  db.execute ("CREATE TABLE IF NOT EXISTS Event (Id TEXT PRIMARY KEY,Date TEXT,Time TEXT,"
-              "Img TEXT, Link TEXT,Title TEXT,Desc TEXT,Venue TEXT, Type TEXT default 'crawl')")
-  try:
-    db.execute ("ALTER TABLE Event ADD COLUMN IF NOT EXISTS VenuePlaintext TEXT");
-  except:
-    print "table alter skipped"
-  
+  if os.path.exists(database_file_path):
+    print database_file_path +" found." 
+  else:
+    db = sqlite3.connect(database_file_path)
+    db.execute ('CREATE TABLE IF NOT EXISTS EventGenres (id INT PRIMARY KEY, name TEXT)')
+    for idx, val  in enumerate(['exhibition','film','concert']):
+      key = idx+1
+      db.execute("INSERT INTO EventGenres VALUES (?,?)",(key, val))
 
-  db.execute ("CREATE INDEX IF NOT EXISTS idx1 ON Event(Venue)")
-  db.execute ("CREATE INDEX IF NOT EXISTS idx2 ON Event(Link)")
-  db.execute ("CREATE INDEX IF NOT EXISTS idx_date ON Event(Date)")
+    db.execute ('CREATE TABLE IF NOT EXISTS EventDates (id TEXT PRIMARY KEY,date TEXT)')
+    db.execute ("CREATE TABLE IF NOT EXISTS Event (id TEXT PRIMARY KEY, startDate TEXT, endDate TEXT, time TEXT,"
+                "img TEXT, link TEXT, title TEXT, desc TEXT, venue TEXT, genre int, type TEXT default 'crawl')")
+    try:
+      db.execute ("ALTER TABLE Event ADD COLUMN IF NOT EXISTS venuePlainText TEXT");
+    except:
+      print "table alter skipped"
+    
 
+    db.execute ("CREATE INDEX IF NOT EXISTS idx1 ON Event(Venue)")
+    db.execute ("CREATE INDEX IF NOT EXISTS idx2 ON Event(Link)")
+    db.execute ("CREATE INDEX IF NOT EXISTS idx_date ON Event(startDate)")
+    db.execute ("CREATE INDEX IF NOT EXISTS idx2_date ON Event(endDate)")
+    
+    print database_file_path +" created." 
 
 main()
